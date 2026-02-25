27c031debe0b61b0510d242b0c51f80fb8782022
Nama Anda <radityapfarma15@gmail.com>
2026-02-25 07:57:59 +0700

    laman verifikasi email, refactor auth view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:app1/views/login_view.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:app1/views/register_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -16,6 +16,11 @@ void main() async {
         primarySwatch: Colors.blue,
       ),
       home: const LoginView(),
+      routes: {
+        '/home': (context) => const HomePage(),
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -25,11 +30,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
@@ -44,36 +45,10 @@ class HomePage extends StatelessWidget {
               //   }                  
               return const LoginView();
             default:
-              return const Text('Loading...');
-          }
-          
+              return const CircularProgressIndicator();
+          }          
         },
-      ),
-    
-    );
+      );
   }
 } 
 
-
-class VerifyEmailView extends StatefulWidget {
-  const VerifyEmailView({super.key});
-
-  @override
-  State<VerifyEmailView> createState() => _VerifyEmailViewState();
-}
-
-class _VerifyEmailViewState extends State<VerifyEmailView> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Please verify your email'),
-        TextButton(onPressed: () async {
-          final user = FirebaseAuth.instance.currentUser;
-          await user?.sendEmailVerification();
-        }, 
-        child: const Text('Send email verification'))
-      ],
-    );
-  }
-}
