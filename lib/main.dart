b188ecb7b4b4eb2f7af23134c5c0d953c7fa0651
Nama Anda <radityapfarma15@gmail.com>
2026-02-25 08:18:28 +0700

    implementasi pengecekan status verifikasi email

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:app1/views/login_view.dart';
 import 'package:app1/views/register_view.dart';
+import 'package:app1/views/verify_email_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -37,13 +39,17 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {                        
             case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              //   if (user?.emailVerified ?? false) {
-              //     return const Text('Done');                
-              //   } else {                  
-              //     return const VerifyEmailView();
-              //   }                  
-              return const LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              if (user != null) {
+                if(user.emailVerified) {
+                  print('Email is verified');
+                } else {
+                  return const VerifyEmailView();
+                }
+              } else {
+                return const LoginView();
+              }
+              return const Text('Done');
             default:
               return const CircularProgressIndicator();
           }          
