759da74f537bcbe06be9dc2e7160b9fcd41e2bc2
Nama Anda <radityapfarma15@gmail.com>
2026-03-03 14:44:31 +0700

    Migrasi ke Auth Service

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,9 +1,14 @@
 import 'package:app1/services/auth/auth_provider.dart';
 import 'package:app1/services/auth/auth_user.dart';
+import 'package:app1/services/auth/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
+
+  factory AuthService.firebase() => AuthService(
+    FirebaseAuthProvider(),
+  );
 
   @override
   Future<AuthUser> createUser({
