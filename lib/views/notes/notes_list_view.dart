bbe7b7d761bdf745d0ea421cd87fa14458858306
Nama Anda <radityapfarma15@gmail.com>
2026-03-09 22:11:11 +0700

    Migrasi ke Layanan Firestore

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -1,12 +1,12 @@
-import 'package:app1/services/crud/notes_service.dart';
+import 'package:app1/services/cloud/cloud_note.dart';
 import 'package:app1/utilities/dialogs/delete_dialog.dart';
 import 'package:flutter/material.dart';
 
-typedef NoteCallback = void Function(DatabaseNote note);
-typedef TapNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(CloudNote note);
+typedef TapNoteCallback = void Function(CloudNote note);
 
 class NotesListView extends StatelessWidget {
-  final List<DatabaseNote> notes;
+  final Iterable<CloudNote> notes;
   final NoteCallback onDeleteNote; 
   final NoteCallback onTap;
   
@@ -23,7 +23,7 @@ class NotesListView extends StatelessWidget {
     return ListView.builder(
       itemCount: notes.length,
       itemBuilder: (context, index) {  
-        final note = notes[index];                         
+        final note = notes.elementAt(index);                         
         return ListTile(
           onTap: () {
             onTap(note);
