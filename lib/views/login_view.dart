e7117f64ce60a91e9115d3352fdee517c28838a6
Nama Anda <radityapfarma15@gmail.com>
2026-03-10 23:17:25 +0700

    tampilan loading

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -39,19 +39,6 @@ class _LoginViewState extends State<LoginView> {
     return BlocListener<AuthBloc, AuthState> (
       listener: (context, state) async{
         if (state is AuthStateLoggedOut) {
-
-          final closeDialog = _closeDialogHandle;
-
-          if (!state.isLoading && closeDialog != null) {
-            closeDialog();
-            _closeDialogHandle = null;
-          } else if (state.isLoading && closeDialog == null) {
-            _closeDialogHandle = showLoadingDialog(
-              context: context,
-              text: 'Loading...',
-            );
-          }
-
           if (state.exception is UserNotFoundAuthException) {
             await showErrorDialog(context, 'Pengguna tidak ditemukan');
           } else if (state.exception is WrongPasswordAuthException) {
